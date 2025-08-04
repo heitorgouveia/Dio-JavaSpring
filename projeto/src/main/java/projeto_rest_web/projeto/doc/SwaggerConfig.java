0e21fd164677d78fbb3dd32245d20567b091da86
Heitor G. Nunes <heitorgouveianunes@gmail.com>
2025-08-04 13:18:07 -0300

    Documentando API com Swagger

---
+++ projeto/src/main/java/projeto_rest_web/projeto/doc/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package projeto_rest_web.projeto.doc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+
+@Configuration
+public class SwaggerConfig {
+
+   @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Minha API")
+                .description("Descrição da API")
+                .version("1.0")
+                .contact(new Contact()
+                    .name("Heitor")
+                    .email("heitor@example.com")
+                    .url("http//seusite.com.br")
+                )
+            );
+    }
+}
