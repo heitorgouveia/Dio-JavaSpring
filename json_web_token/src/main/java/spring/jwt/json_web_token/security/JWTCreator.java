8bf060640cca1b212173cb6852d97a968b153f92
Heitor G. Nunes <heitorgouveianunes@gmail.com>
2025-08-16 20:33:47 -0300

    Json web token continuação

---
+++ json_web_token/src/main/java/spring/jwt/json_web_token/security/JWTCreator.java
@@ -0,0 +1,42 @@
+package spring.jwt.json_web_token.security;
+
+import java.util.List;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+public class JWTCreator {
+
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String ROLES_AUTHORITIES = "authorities";
+
+    private static List<String> checkRoles(List<String> roles) {
+        // You can add validation or transformation logic here if needed
+        return roles;
+    }
+
+    public static String create(String prefix, String key, JWTObject jwtObject) {
+        String token = Jwts.builder().setSubject(jwtObject.getSubject()).setIssuedAt(jwtObject.getIssuedAt()).setExpiration(jwtObject.getExpiration())
+                .claim(ROLES_AUTHORITIES, checkRoles(jwtObject.getRoles())).signWith(SignatureAlgorithm.HS512, key).compact();
+        return prefix + " " + token;
+
+}
+
+public static JWTObject create(String token, String prefix, String key)
+        throws ExpiredJwtException, UnsupportedJwtException, IllegalArgumentException, MalformedJwtException, SignatureException {
+        JWTObject jwtObject = new JWTObject();
+        token = token.replace(prefix + " ", "");
+        Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        jwtObject.setSubject(claims.getSubject());
+        jwtObject.setIssuedAt(claims.getIssuedAt());
+        jwtObject.setExpiration(claims.getExpiration());
+        jwtObject.setRoles((List<String>) claims.get(ROLES_AUTHORITIES));
+        return jwtObject;
+    }
+
+}
