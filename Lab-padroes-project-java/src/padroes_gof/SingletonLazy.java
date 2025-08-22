245f7349bb62f1b0406b61e9b5a1347249fb2e7a
Heitor G. Nunes <heitorgouveianunes@gmail.com>
2025-08-22 20:44:29 -0300

    Introdução à singleton padroes_gof

---
+++ Lab-padroes-project-java/src/padroes_gof/SingletonLazy.java
@@ -0,0 +1,23 @@
+package padroes_gof;
+
+/*Singleton preguiçoso
+ * Implementa o padrão Singleton de forma que a instância é criada apenas quando é realmente necessária.
+ * Isso é útil para economizar recursos, especialmente se a instância é pesada ou rara de ser usada.
+ */
+public class SingletonLazy {
+
+    private static SingletonLazy instancia;
+
+    private SingletonLazy(){
+        super();
+    }
+
+    public static SingletonLazy getInstancia(){
+        if(instancia == null){
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+    
+    
+}
---
+++ Lab-padroes-project-java/src/padroes_gof/SingletonLazyHolder.java
@@ -0,0 +1,21 @@
+package padroes_gof;
+
+/*Singleton Lazy Holder
+ */
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder{
+        private static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder(){
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+
+    
+    
+}
