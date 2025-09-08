65da912eafe606e7660d8f9da327e5b06f023333
Heitor G. Nunes <heitorgouveianunes@gmail.com>
2025-09-08 18:28:07 -0300

    Testando aplicação

---
+++ lab/src/main/java/padroes_gof/lab/service/ViaCepService.java
@@ -0,0 +1,16 @@
+package padroes_gof.lab.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import padroes_gof.lab.model.Endereco;
+
+@FeignClient(name = "viacep", url ="viacep.com.br/ws/01001000/json/")
+public interface ViaCepService {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
+}
