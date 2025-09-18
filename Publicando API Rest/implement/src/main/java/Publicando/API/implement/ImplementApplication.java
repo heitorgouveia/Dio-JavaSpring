d38ce1375e380b98127223e924a3bf02474e8394
Heitor G. Nunes <heitorgouveianunes@gmail.com>
2025-09-17 22:16:25 -0300

    Implementando API Rest

---
+++ Publicando API Rest/implement/src/main/java/Publicando/API/implement/ImplementApplication.java
@@ -0,0 +1,13 @@
+package Publicando.API.implement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementApplication.class, args);
+	}
+
+}
---
+++ Publicando API Rest/implement/src/test/java/Publicando/API/implement/ImplementApplicationTests.java
@@ -0,0 +1,13 @@
+package Publicando.API.implement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
